ac5e3d682f1efc26f8ee8e4067a9c7234e6e367e
daljo25 <camalion007@gmail.com>
2025-09-12 12:32:47 +0200

    fix onBrokenLinks para poder desplegar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
   organizationName: "filamentenespanol", // Usually your GitHub org/user name.
   projectName: "documentacion", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
