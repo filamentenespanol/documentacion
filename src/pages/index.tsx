7c9e757a0475b074d9a4050b8f1b18a6969f8c43
daljo25 <camalion007@gmail.com>
2025-10-16 12:52:00 +0200

    🐞 fix: Barra de Progreso actualizada al 49%

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home(): ReactNode {
       description="Documentacion no oficial de Filamentphp en español">
       <HomepageHeader />
       <main>
-        <TranslationProgress progress={32} />
+        <TranslationProgress progress={49} />
         <HomepageFeatures />
       </main>
     </Layout>
